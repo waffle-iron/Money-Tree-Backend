43c23a0a33edcca16211e1f28bd966b4f35d3f87
bhargav00 <bmaniar00@gmail.com>
2017-03-05 22:35:55 +0530

    Login logic redefined

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var express = require("express");
 var session = require('express-session');
 var bodyParser = require('body-parser');
-var sess; //session variable
+var sess; //global session variable
 //Below command is to connect with database
 var connection = mysql.createConnection({
 
@@ -10,6 +10,15 @@ var connection = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'moneytree'
+});
+//Creating Pool of connection
+var pool = mysql.createPool({
+    connectionLimit: 100, //important
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'moneytree',
+    debug: false
 });
 var app = express();
 //secret key is must to set to working with sesssions in express
@@ -20,47 +29,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 //Below code is to enable CORS in node server
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-//DB connection function
-function db_connect() {
-    connection.connect(function (err) {
-        if (!err) {
-            console.log("Database is connected ... nn");
-        } else {
-            console.log("Error connecting database ... nn");
-        }
-    });
-}
-//Server listen port  is 3000
-app.listen(3000);
-console.log("Server started on 3000 ... nn");
-//login post method
-app.post('/login', function (req, res) {
+
+function login(req, res) {
     sess = req.session;
+    console.log('got request');
     if (req.body.username == null || req.body.password == null) {
         res.json({
             status: 'incomplete data'
         });
         res.end();
-    } else {
-        db_connect();
+    }
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            res.json({
+                "code": 100,
+                "status": "Error in connection database"
+            });
+            return;
+        }
         connection.query('SELECT * from login where username="' + req.body.username + '"and password="' + req.body.password + '"',
             function (err, rows, fields) {
-                connection.end();
+                connection.release();
                 if (!err) {
                     if (rows.length == 1) {
                         //setting session variables
-                        sess.access_type = rows.access_type;
-                        sess.pm_id = rows.pm_id;
-                        sess.et_id = rows.et_id;
-                        sess.login='true';
+                        sess.access_type = rows[0].access_type;
+                        sess.pm_id = rows[0].pm_id;
+                        sess.et_id = rows[0].et_id;
+                        sess.login = 'true';
                         res.json({
-                            status: 'succesful'
+                            status: 'succesful',
+                            access_type: rows[0].access_type
                         });
                         res.end();
                     } else {
@@ -74,5 +80,19 @@ app.post('/login', function (req, res) {
                     res.end();
                 }
             });
-    }
+        connection.on('error', function (err) {
+            res.json({
+                "status": "Error in connection database"
+            });
+            return;
+        });
+    });
+}
+
+//Server listen port  is 3000
+app.listen(3000);
+console.log("Server started on 3000 ... nn");
+//login post method
+app.post('/login', function (req, res) {
+    login(req, res);
 });
